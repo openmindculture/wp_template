3a53fdf468036b8a3cec3e6a171d8adf8ec2413b
Ingo Steinke <ingosteinke@gmx.de>
2023-03-27 13:18:59 +0200

    postcss import configuration

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const postcssCustomProperties = require('postcss-custom-properties');
+module.exports = {
+	plugins: [
+		require('postcss-import'),
+		postcssCustomProperties({
+			preserve: false,
+		}),
+		/* require('postcss-minify'), */
+	]
+}
