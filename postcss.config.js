f5edef791acc1398dfd6d37b6acae4955cb7b167
Ingo Steinke <ingosteinke@gmx.de>
2023-03-28 14:14:52 +0200

    extends at rule, formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,6 @@
-const postcssCustomProperties = require('postcss-custom-properties');
+const postcssCustomProperties = require(
+  'postcss-custom-properties'
+);
 module.exports = {
 	plugins: [
 		require('postcss-import'),
@@ -6,6 +8,7 @@ module.exports = {
 			preserve: false,
 		}),
 		require('postcss-nesting'),
+    require('postcss-extend-rule'),
 		require('postcss-minify'),
 	],
 }
